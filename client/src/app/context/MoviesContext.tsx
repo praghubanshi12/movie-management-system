b1c3f5e907591b403c9b046ecb0bb3031cd8f8e1
unknown <praghubanshi12@gmail.com>
2024-10-17 18:09:26 +0545

    typescript type added

--- client/src/app/context/MoviesContext.tsx
+++ client/src/app/context/MoviesContext.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { initialMovieFormData } from "../const/const";
 import { Movie } from "../model/movie";
 
-export const MoviesContext = React.createContext('');
-export const MoviesContextProvider = ({ children }) => {
+export const MoviesContext = React.createContext({});
+export const MoviesContextProvider = ({ children } : {children: ReactNode} ) => {
     const [movies, setMovies] = useState<Movie[]>([]);
   /* context api */ const [movieFormDetails, setMovieFormDetails] = useState({
     mode: "",
